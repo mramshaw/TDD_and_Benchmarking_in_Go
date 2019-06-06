a01527942164b7bcd77f63d033dbfe9fc1edd9cf
mramshaw <mramshaw@alumni.concordia.ca>
2019-06-06 18:40:55 -0400

    Fix formatting

--- stringutil/better_reverse.go
+++ stringutil/better_reverse.go
@@ -3,12 +3,12 @@ package stringutil
 // BetterReverse returns its argument string reversed rune-wise left to right.
 func BetterReverse(s string) string {
 	r := []rune(s)
-    n := len(r)
+	n := len(r)
 	rOut := make([]rune, n)
 	i := n - 1
-    for _, c := range s {
+	for _, c := range s {
 		rOut[i] = c
-        i -= 1
-    }
+		i -= 1
+	}
 	return string(rOut)
 }
--- stringutil/naive_reverse.go
+++ stringutil/naive_reverse.go
@@ -3,10 +3,10 @@ package stringutil
 // NaiveReverse returns its argument string reversed rune-wise left to right.
 func NaiveReverse(s string) string {
 	r := []rune(s)
-    n := len(r)
+	n := len(r)
 	rOut := make([]rune, n)
 	for i := 0; i < n; i += 1 {
-		rOut[i] = r[n - 1 - i]
+		rOut[i] = r[n-1-i]
 	}
 	return string(rOut)
 }
